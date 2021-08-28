83311b681906b46b4927df101f57254b8863f928
Daniel Harper <daniel.c.harper@live.com>
2021-08-28 12:26:52 -0600

    Initial Commit

---
+++ build/CMakeFiles/germanytrip.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/germanytrip.dir/main.cpp.o"
+  "germanytrip.pdb"
+  "germanytrip"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/germanytrip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
